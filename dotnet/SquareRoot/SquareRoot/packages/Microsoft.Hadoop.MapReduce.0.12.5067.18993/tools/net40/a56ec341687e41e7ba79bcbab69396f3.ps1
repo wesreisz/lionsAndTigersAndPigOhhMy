e49afbd0f497841b94395ed277f284c7f0df3ee5
wesreisz <wes@wesleyreisz.com>
2013-11-19 21:55:47 -0800

    moved files around

--- dotnet/SquareRoot/SquareRoot/packages/Microsoft.Hadoop.MapReduce.0.12.5067.18993/tools/net40/a56ec341687e41e7ba79bcbab69396f3.ps1
+++
@@ -1,9 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-Write-Host Setting MRLib items CopyToOutputDirectory=true
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.CombineDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1;
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1;
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.MapReduce.dll").Properties.Item("CopyToOutputDirectory").Value = 1;
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.Hadoop.ReduceDriver.exe").Properties.Item("CopyToOutputDirectory").Value = 1;
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("Microsoft.WindowsAzure.Management.HDInsight.Framework.Experimental.Threading.dll").Properties.Item("CopyToOutputDirectory").Value = 1;
-$project.ProjectItems.Item("MRLib").ProjectItems.Item("MRRunner.exe").Properties.Item("CopyToOutputDirectory").Value = 1;
--- dotnet/SquareRoot/SquareRoot/packages/Microsoft.Hadoop.MapReduce.0.12.5067.18993/tools/net40/install.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$dir = split-Path $MyInvocation.MyCommand.Path;
-& "$dir\a56ec341687e41e7ba79bcbab69396f3.ps1" $installPath $toolsPath $package $project;
-
